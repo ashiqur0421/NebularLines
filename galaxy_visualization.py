0df465cf7186d52666471031cb46cd144b22a9c0
Braden Nowicki <91173837+BradenN6@users.noreply.github.com>
2024-12-03 17:29:02 -0500

    generalizing scripts to prepare to run on many time slices

--- galaxy_visualization.py
+++ galaxy_visualization.py
@@ -1,19 +1,13 @@
 '''
-Braden Nowicki with Dr. Massimo Ricotti
-University of Maryland, College Park Astronomy Department
+Braden Nowicki
 
-Script to visualize RAMSES-RT Simulations of high-redshift galaxies in a variety of metal lines. 
-Ionization Parameter, Number Density, and Temperature for each pixel are input into an interpolator 
-for each line; the interpolator is created via the module 'emission.py'. 'emission.py' currently 
-uses the 'linelist.dat' datatable to build interpolators; this can be adjusted to work with other 
-tables from Cloudy runs. 
+Visualization routines for simulation datasets.
 '''
 
 '''
-Setup fields in yt
+Projection, Slice Plot Routines
 '''
 
-# TODO clean imports (only those necessary for main)
 # importing packages
 #import pandas as pd
 import numpy as np
@@ -29,12 +23,6 @@ from astropy.cosmology import FlatLambdaCDM
 from matplotlib.colors import LogNorm
 import sys
 
-#f1 = "/Users/bnowicki/Documents/Research/Ricotti/output_00273"
-filename = sys.argv[1]
-
-
-# TODO user input which fields to plot, width, etc.
-
 # Cloudy Grid Run Bounds (log values)
 # Umin, Umax, Ustep: -6.0 1.0 0.5
 # Nmin, Nmax, Nstep: -1.0 6.0 0.5 
@@ -49,77 +37,6 @@ wavelengths=[6562.80, 1304.86, 6300.30, 3728.80, 3726.10, 1660.81, 1666.15, \
              4363.21, 4958.91, 5006.84, 1640.41, 1335.66, \
              1906.68, 1908.73, 1549.00, 2795.53, 2802.71, 3868.76, \
              3967.47, 1238.82, 1242.80, 1486.50, 1749.67, 6716.44, 6730.82]
-             
-# Ionization Parameter Field
-# Based on photon densities in bins 2-4
-# Don't include bin 1 -> Lyman Werner non-ionizing
-def _ion_param(field, data): 
-    from yt.frontends.ramses.field_handlers import RTFieldFileHandler
-    p = RTFieldFileHandler.get_rt_parameters(ds).copy()
-    p.update(ds.parameters)
-
-    cgs_c = 2.99792458e10     #light velocity
-    pd_2 = data['ramses-rt','Photon_density_2']*p["unit_pf"]/cgs_c #physical photon number density in cm-3
-    pd_3 = data['ramses-rt','Photon_density_3']*p["unit_pf"]/cgs_c
-    pd_4 = data['ramses-rt','Photon_density_4']*p["unit_pf"]/cgs_c
-
-    photon = pd_2 + pd_3 + pd_4
-
-    return photon/data['gas', 'number_density']  
-
-# Luminosity field
-# Cloudy Intensity obtained assuming height = 1cm
-# Return intensity values erg/s/cm**2
-# Multiply intensity at each pixel by volume of pixel -> luminosity
-def get_luminosity(line):
-   def _luminosity(field, data):
-      return data['gas', 'intensity_' + line]*data['gas', 'volume']
-   return copy.deepcopy(_luminosity)
-
-yt.add_field(
-    ('gas', 'ion-param'), 
-    function=_ion_param, 
-    sampling_type="cell", 
-    units="cm**3", 
-    force_override=True
-)
-
-# True divides emissions by density squared in interpolator
-dens_normalized = True
-if dens_normalized: 
-    units = '1/cm**6'
-else:
-    units = '1'
-
-# Add intensity and luminosity fields for all lines in the list
-for i in range(len(lines)):
-    yt.add_field(
-        #('gas', 'intensity_' + lines[i] + '_[erg_cm^{-2}_s^{-1}]'),
-        ('gas', 'intensity_' + lines[i]),
-        function=emission.get_line_emission(i, dens_normalized),
-        sampling_type='cell',
-        units=units,
-        force_override=True
-    )
-    
-    yt.add_field(
-        ('gas', 'luminosity_' + lines[i]),
-        function=get_luminosity(lines[i]),
-        sampling_type='cell',
-        units='1/cm**3',
-        force_override=True
-    )
-
-# Load Simulation Data
-ds = yt.load(filename)
-ad = ds.all_data()
-
-# For projections in a spherical region
-# TODO generalize center
-#sp = ds.sphere([0.49118094, 0.49275361, 0.49473726], (2000, "pc"))
-
-'''
-Projection, Slice Plot Routines
 
 
 # TODO
@@ -184,11 +101,10 @@ def convert_to_plt(data_file, yt_plot, plot_type, field, lbox, title):
 
     # TODO fix plots, title
 
-
-
+'''
 Projection Plots of Ionization Parameter, Number Density, 
 Mass Density, Temperature, Metallicity
-
+'''
 
 # Wrapper for making diagnostic plots of given fields
 def plot_diagnostics(ds, sp, file, center, width):
@@ -222,9 +138,9 @@ def plot_diagnostics(ds, sp, file, center, width):
     proj_metallicity.save(str(width) + 'pc_')
     convert_to_plt(file, proj_metallicity, 'proj', 'metallicity', width, 'Metallicity')
 
-
+'''
 Visualizing Line Intensities
-
+'''
 
 # TODO expand to more lines
 def plot_intensities(ds, sp, file, center, width):
@@ -249,9 +165,9 @@ def plot_intensities(ds, sp, file, center, width):
     convert_to_plt(file, slc_halpha, 'slc', 'intensity_H1_6562.80A', width, r'H1 6562.80A Intensity [$erg\: s^{-1}\: cm^{-2}$]')
 
 
-
+'''
 Plot Spectra at simulated wavelengths
-
+'''
 
 def spectra_driver(ds, ad, data_file):
     luminosities=[]
@@ -355,36 +271,3 @@ def plot_voigts(centers, amplitudes, sigmas, gammas, noise_lvl):
 # where most of signal coming from - order cells by luminosity
 # full calculation for brightest
 # or sum cells within temp range - treat all as having same width. bulk velocity problem
-
-# star center of mass
-'''
-
-
-# 
-def sim_diagnostics(ds, data_file):
-    #ds = yt.load(data_file)
-    #ad = ds.all_data()
-
-    # center derived from max density pixel
-    # Alternatively, use
-    # center=("max", ("gas", "[field]")),
-    # in projection plot
-    center_max=[0.49118094, 0.49275361, 0.49473726]
-    # TODO star center of mass
-    
-    # For projections in a spherical region
-    sp = ds.sphere(center_max, (2000, "pc"))
-    width = 400
-
-    plot_diagnostics(ds, sp, data_file, center_max, width)
-    plot_intensities(ds, sp, data_file, center_max, width)
-    spectra_driver(ds, ad, data_file)
-
-sim_diagnostics(ds, filename)
-
-# TODO cleanup script
-# Shell script to run it on all input files
-# run in zaratan
-
-# TODO save into a certain file for the dataset
-# TODO save table of luminosities for each line
